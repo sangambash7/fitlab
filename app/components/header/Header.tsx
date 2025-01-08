873ee4bca9af989b7706554278796d87bb116c08
sangambash7 <san.gambashidze@gmail.com>
2025-01-08 17:09:03 +0200

    added logo, responsive menuitems in header

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,5 +1,57 @@
+import Link from "next/link";
+import Logo from "./Logo";
+import Sidebar from "./Sidebar";
+
 function Header() {
-  return <header className="font-bold">This is Header</header>;
+  return (
+    <header className="bg-[#1B4A8E] text-white w-full text-lg flex justify-center py-2">
+      <div className="w-full xl:w-[1200px] mx-1">
+        <div className="flex justify-between items-center">
+          <Logo />
+          {/* Nav Items */}
+          <div className="hidden md:block">
+            <ul className="flex gap-3">
+              <li>
+                <button className="hover:text-[#DDD]">
+                  <Link href={"/"}>Home</Link>
+                </button>
+              </li>
+              <li>
+                <button className="hover:text-[#DDD]">
+                  <Link href={"/clubs"}>Clubs</Link>
+                </button>
+              </li>
+              <li>
+                <button className="hover:text-[#DDD]">
+                  {" "}
+                  <Link href={"/shop"}>Shop</Link>
+                </button>
+              </li>
+              <li>
+                <button className="hover:text-[#DDD]">
+                  <Link href={"/contact"}>Contact</Link>
+                </button>
+              </li>
+              <li>
+                <button className="hover:text-[#DDD]">
+                  <Link href={"/about"}>About Us</Link>
+                </button>
+              </li>
+            </ul>
+          </div>
+          <div className="flex gap-3 items-center">
+            <button className="bg-slate-100 text-blue-700 rounded-md py-1 px-4">
+              Login
+            </button>
+            <button className="bg-blue-700 text-white rounded-md py-1 px-4">
+              Join
+            </button>
+            <Sidebar />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
---
+++ app/components/header/Sidebar.tsx
@@ -0,0 +1,66 @@
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+import { AiOutlineMenu } from "react-icons/ai";
+import { MdClose } from "react-icons/md";
+
+function Sidebar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <div className="relative md:hidden">
+        {!isOpen ? (
+          <button
+            onClick={() => setIsOpen(true)}
+            className="md:hidden text-2xl"
+          >
+            <AiOutlineMenu />
+          </button>
+        ) : (
+          <button
+            onClick={() => setIsOpen(false)}
+            className="md:hidden text-2xl"
+          >
+            <MdClose />
+          </button>
+        )}
+      </div>
+
+      {isOpen && (
+        <div className="md:hidden w-full absolute left-0 top-12 z-10 bg-[#1B4A8E]">
+          <ul className="flex flex-col gap-3 ms-3">
+            <li>
+              <button className="hover:text-[#DDD]">
+                <Link href={"/"}>Home</Link>
+              </button>
+            </li>
+            <li>
+              <button className="hover:text-[#DDD]">
+                <Link href={"/clubs"}>Clubs</Link>
+              </button>
+            </li>
+            <li>
+              <button className="hover:text-[#DDD]">
+                {" "}
+                <Link href={"/shop"}>Shop</Link>
+              </button>
+            </li>
+            <li>
+              <button className="hover:text-[#DDD]">
+                <Link href={"/contact"}>Contact</Link>
+              </button>
+            </li>
+            <li>
+              <button className="hover:text-[#DDD]">
+                <Link href={"/about"}>About Us</Link>
+              </button>
+            </li>
+          </ul>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default Sidebar;
