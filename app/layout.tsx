efeb86a6cf70d57dc27c85723bb932068a6b4acc
sangambash7 <san.gambashidze@gmail.com>
2025-01-10 19:37:56 +0200

    created home intro responsive with header

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={rubik.className}>
+      <body className={`${rubik.className} relative`}>
         <Header />
         {children}
         <Footer />
