68ee6a99e4301887771a3994ea9d6ffb77e97ec0
sangambash7 <san.gambashidze@gmail.com>
2025-01-31 19:05:17 +0200

    created footer

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,5 +1,34 @@
+import Image from "next/image";
+
 function Footer() {
-  return <footer>This is Footer</footer>;
+  return (
+    <footer className="bg-[#1B4A8E] flex justify-center">
+      <div className="w-[1200px] mx-1 text-white">
+        <div className="grid grid-cols-6 pt-6 pb-4 items-center">
+          <div className="col-span-3">
+            <ul className="flex gap-4">
+              <li>Contact</li>
+              <li>About Our Company</li>
+              <li>Clubs</li>
+              <li>Pricing</li>
+            </ul>
+          </div>
+          <div className="col-span-3">
+            <div className="flex items-center gap-4">
+              <Image src="/logo.png" width={120} height={120} alt="Logo" />
+
+              <div className="italic text-[20px]">
+                FitLab&apos;s mission is to empower individuals to achieve their
+                fitness goals by providing top-tier gym facilities and
+                high-quality equipment, ensuring a seamless and rewarding
+                workout experience for all.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
