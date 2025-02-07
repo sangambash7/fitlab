8179813d12fa8a931a37096dfee5c915cb664919
sangambash7 <san.gambashidze@gmail.com>
2025-02-07 17:35:04 +0200

    small changes: navbar list corrections

--- app/[locale]/components/header/AuthArea.tsx
+++ app/[locale]/components/header/AuthArea.tsx
@@ -52,8 +52,11 @@ async function AuthArea() {
                 </Link>
               </DropdownMenuItem>
               <DropdownMenuItem>
-                <Link href={"/profile/orders"} className="text-[1rem] w-full">
-                  {t("Orders")}
+                <Link
+                  href={"/profile/fitlabPass"}
+                  className="text-[1rem] w-full"
+                >
+                  Fitlab Pass
                 </Link>
               </DropdownMenuItem>
               <DropdownMenuSeparator />
